08b30b9c2b891cead74a7c72fee485431dfe05ad
Adam Streu <adamstreu@gmail.com>
2019-05-08 10:59:35 -0700

    directory changes

---
+++ architecture/main.tf
@@ -0,0 +1,34 @@
+# Main.tf
+
+# Set up AWS
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+# Deploy storage resources
+module "storage" {
+  source       = "./storage"
+  project_name = "${var.project_name}"
+}
+
+# Deploy networking resources
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = "${var.vpc_cidr}"
+  accessip     = "${var.accessip}"
+  public_cidrs = "${var.public_cidrs}"
+}
+
+# Deploy compute resources
+module "compute" {
+  source          = "./compute"
+  public_key_path = "${var.public_key_path}"
+  public_key_name = "${var.public_key_name}"
+  instance_count  = "${var.instance_count}"
+  instance_type   = "${var.instance_type}"
+  subnets         = "${module.networking.public_subnets}"
+  security_group  = "${module.networking.public_sg}"
+  subnet_ips      = "${module.networking.subnet_ips}"
+}
---
+++ architecture/variables.tf
@@ -0,0 +1,56 @@
+# --- Networking Variables ---
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.123.0.0/16"
+}
+
+variable "public_cidrs" {
+  type = "list"
+  default = [
+    "10.123.1.0/24",
+    "10.123.2.0/24",
+  ]
+}
+
+variable "accessip" {
+  type = "list"
+  default = [
+    "0.0.0.0/0",
+    "0.0.0.0/0",
+  ]
+}
+
+# --- Storage Variables ---
+
+variable "project_name" {
+  default = "immutable_kubernetes_build"
+}
+
+# --- Compute  variables ---
+
+variable "instance_count" {
+  default     = 2
+  description = "Number of compute instances to create"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "Size and type of compute instance to create"
+}
+
+variable "public_key_name" {
+  default = "terraform_aws_key"
+}
+
+
+
+# --- Leave following AWS variable declarations as is and edit in terraform.tfvars ---
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "public_key_path" {}
+
